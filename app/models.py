e731f105e37fc5146c9d971f41d3230300653212
dlcastra <1dlcastra@gmail.com>
2024-03-31 19:17:55 +0200

    Alter fields

--- app/models.py
+++ app/models.py
@@ -8,16 +8,22 @@ class Client(models.Model):
     phone = models.CharField(max_length=20, blank=True)
     email = models.EmailField(max_length=255)
 
+    def __str__(self):
+        return f"ID: {self.id} | Client: {self.name} {self.surname}"
+
 
 class Lawyer(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     birthdate = models.DateField()
     experience = models.IntegerField()
-    successful_cases = models.IntegerField()
-    unsuccessful_cases = models.IntegerField()
+    successful_cases = models.PositiveIntegerField(default=0)
+    unsuccessful_cases = models.PositiveIntegerField(default=0)
     price = models.PositiveIntegerField()
     characterization = models.TextField()
+
+    def __str__(self):
+        return f"ID: {self.id} | Lawyer: {self.name} {self.surname}"
 
 
 class CaseForm(models.Model):
@@ -32,3 +38,4 @@ class Case(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="client_lawyer")
     case_closed_successfully = models.BooleanField()
     article = models.CharField(max_length=255)
+    description = models.TextField(null=True)
