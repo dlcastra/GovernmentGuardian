22591b1aaef134b5650e97b6cc5601a64ee451e3
dlcastra <1dlcastra@gmail.com>
2024-04-01 18:57:26 +0200

    Added forms for the Clint, Lawyer and Case models and also added validation for their fields

---
+++ app/forms.py
@@ -0,0 +1,161 @@
+import phonenumbers
+from django import forms
+
+from app.models import Client, Lawyer, Case
+
+
+class ClientForm(forms.ModelForm):
+    class Meta:
+        model = Client
+        fields = ["name", "surname", "birthdate", "phone", "email"]
+        labels = {"name": "Name", "surname": "Surname", "Birthdate": "Birthdate", "phone": "Phone", "email": "Email"}
+
+    def clean_name(self):
+        client_name = self.cleaned_data["name"]
+        if len(client_name) < 1:
+            raise forms.ValidationError("Name is too short")
+
+        if len(client_name) > 50:
+            raise forms.ValidationError("Name is too long")
+
+        if any(char.isdigit() for char in client_name):
+            raise forms.ValidationError("Name must not contain digits")
+
+        if client_name[0].islower():
+            change_name = client_name.capitalize()
+            self.cleaned_data["name"] = change_name
+
+        if any(char.isupper() for char in client_name[0:]):
+            changed_name = client_name.capitalize()
+            self.cleaned_data["name"] = changed_name
+
+        return client_name
+
+    def clean_surname(self):
+        client_surname = self.cleaned_data["surname"]
+        if len(client_surname) < 1:
+            raise forms.ValidationError("Surname is too short")
+
+        if len(client_surname) > 50:
+            raise forms.ValidationError("Surname is too long")
+
+        if any(char.isdigit() for char in client_surname):
+            raise forms.ValidationError("Surname is too long")
+
+        if client_surname[0].islower():
+            change_surname = client_surname.capitalize()
+            self.cleaned_data["surname"] = change_surname
+
+        if any(char.isupper() for char in client_surname[0:]):
+            change_surname = client_surname.capitalize()
+            self.cleaned_data["surname"] = change_surname
+
+        return client_surname
+
+    def clean_phone(self):
+        phone = self.cleaned_data["phone"]
+        if not phone:
+            raise forms.ValidationError("Phone cannot be empty")
+
+        if len(phone) < 9:
+            raise forms.ValidationError("The number provided is too short")
+
+        try:
+            parsed = phonenumbers.parse(phone, None)
+        except phonenumbers.NumberParseException as error:
+            raise forms.ValidationError(f"{error.args[0]}")
+
+        formatted_phone = phonenumbers.format_number(parsed, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+        return formatted_phone
+
+
+class LawyerForm(forms.ModelForm):
+    class Meta:
+        model = Lawyer
+        fields = [
+            "name",
+            "surname",
+            "birthdate",
+            "experience",
+            "successful_cases",
+            "unsuccessful_cases",
+            "price",
+            "characterization",
+        ]
+        labels = {
+            "name": "Name",
+            "surname": "Surname",
+            "birthdate": "Birthday",
+            "experience": "Experience",
+            "successful_cases": "Successful cases",
+            "unsuccessful_cases": "Unsuccessful cases",
+            "price": "Price",
+            "characterization": "Characterization",
+        }
+
+    def clean_name(self):
+        lawyer_name = self.cleaned_data["name"]
+        if len(lawyer_name) < 1:
+            raise forms.ValidationError("Name is too short")
+
+        if len(lawyer_name) > 50:
+            raise forms.ValidationError("Name is too long")
+
+        if any(char.isdigit() for char in lawyer_name):
+            raise forms.ValidationError("Name must not contain digits")
+
+        if lawyer_name[0].islower():
+            change_name = lawyer_name.capitalize()
+            self.cleaned_data["name"] = change_name
+
+        if any(char.isupper() for char in lawyer_name[0:]):
+            changed_name = lawyer_name.capitalize()
+            self.cleaned_data["name"] = changed_name
+
+        return lawyer_name
+
+    def clean_surname(self):
+        lawyer_surname = self.cleaned_data["surname"]
+        if len(lawyer_surname) < 1:
+            raise forms.ValidationError("Surname is too short")
+
+        if len(lawyer_surname) > 50:
+            raise forms.ValidationError("Surname is too long")
+
+        if any(char.isdigit() for char in lawyer_surname):
+            raise forms.ValidationError("Surname is too long")
+
+        if lawyer_surname[0].islower():
+            change_surname = lawyer_surname.capitalize()
+            self.cleaned_data["surname"] = change_surname
+
+        if any(char.isupper() for char in lawyer_surname[0:]):
+            change_surname = lawyer_surname.capitalize()
+            self.cleaned_data["surname"] = change_surname
+
+        return lawyer_surname
+
+    def clean_price(self):
+        price = self.cleaned_data["price"]
+        if price < 100:
+            raise forms.ValidationError("The price of services cannot be less than 100")
+
+        return price
+
+
+class CaseForm(forms.ModelForm):
+    class Meta:
+        model = Case
+        fields = ["is_active", "lawyer", "client", "case_closed_successfully", "article", "description"]
+        labels = {
+            "is_active": "Now is active",
+            "lawyer": "Lawyer",
+            "client": "Client",
+            "case_closed_successfully": "Completed successfully",
+            "description": "Description",
+        }
+
+    def clean_article(self):
+        article = self.cleaned_data["article"]
+        if len(article) > 255:
+            raise forms.ValidationError("Article is too long")
