d808e90ce2ea9fe334941fe169f2abf3148ebe5b
dlcastra <1dlcastra@gmail.com>
2024-04-01 18:57:56 +0200

    Vies for testing forms

--- app/views.py
+++ app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from app.forms import LawyerForm
+from app.models import Lawyer
+
+
+def register(request):
+    ...
+
+
+def index(request):
+    lawyers = Lawyer.objects.all()
+    return render(request, "index.html", {"lawyers": lawyers})
+
+
+def add_lawyer(request):
+    if request.method == "GET":
+        form = LawyerForm()
+        return render(request, "add_lawyer.html", {"form": form})
+
+    form = LawyerForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect("index")
+
+    return render(request, "add_lawyer.html", {"form": form})
