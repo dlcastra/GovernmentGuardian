4da2378766cdbbcbc9a7dc0a86384230e6b12d18
dlcastra <1dlcastra@gmail.com>
2024-04-03 19:49:08 +0200

    Added the ability to manually register and bind a user to another model. There is still a problem with Auth0 and application models binding

---
+++ users/views.py
@@ -0,0 +1,139 @@
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.django_client import OAuth
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.core.signing import Signer, BadSignature
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from app.forms import ClientForm, LawyerForm
+from users.forms import UserCreationFormWithEmail
+
+""" --- SIGN UP AND SIGN IN --- """
+
+oauth = OAuth()
+
+
+def send_activation_email(request, user: User):
+    user_signed = Signer().sign(user.id)
+    signed_url = request.build_absolute_uri(f"/activate/{user_signed}")
+    send_mail(
+        subject="Registration successfully completed",
+        message=("Click here to activate your account: " + signed_url),
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[user.email],
+        fail_silently=False,
+    )
+
+
+def register_view(request):
+    if request.method == "GET":
+        form = UserCreationFormWithEmail()
+        return render(request, "registration/register.html", {"form": form})
+
+    form = UserCreationFormWithEmail(request.POST)
+    if form.is_valid():
+        form.instance.is_active = False
+        form.save()
+        send_activation_email(request, form.instance)
+        return redirect("login_view")
+
+    return render(request, "registration/register.html", {"form": form})
+
+
+def activate(request, user_signed):
+    try:
+        user_id = Signer().unsign(user_signed)
+    except BadSignature:
+        return redirect("login_view")
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return redirect("login_view")
+    user.is_active = True
+    user.save()
+    return redirect("login_view")
+
+
+oauth.register(
+    "auth0",
+    client_id=settings.AUTH0_CLIENT_ID,
+    client_secret=settings.AUTH0_CLIENT_SECRET,
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f"https://{settings.AUTH0_DOMAIN}/.well-known/openid-configuration",
+)
+
+
+def callback(request):
+    token = oauth.auth0.authorize_access_token(request)
+    print(token)
+    request.session["user"] = token
+
+    email = token.get("email")
+    user, created = User.objects.get_or_create(email=email)
+    request.session["user"] = user.pk
+
+    return redirect(request.build_absolute_uri(reverse("roles")))
+
+
+def login(request):
+    return oauth.auth0.authorize_redirect(request, request.build_absolute_uri(reverse("callback")))
+
+
+def logout(request):
+    request.session.clear()
+
+    return redirect(
+        f"https://{settings.AUTH0_DOMAIN}/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": request.build_absolute_uri(reverse("index")),
+                "client_id": settings.AUTH0_CLIENT_ID,
+            },
+            quote_via=quote_plus,
+        ),
+    )
+
+
+""" --- CLIENT OR LAWYER ---"""
+
+
+def select_role(request):
+    if request.method == "POST":
+        role = request.POST.get("role")
+        if role == "client":
+            return redirect("create_client")
+        elif role == "lawyer":
+            return redirect("create_lawyer")
+
+    return render(request, "roles/select_role.html")
+
+
+def client_registration(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            client = form.save(commit=False)
+            client.user = request.user
+            client.save()
+            return redirect('list')
+    else:
+        form = ClientForm()
+    return render(request, 'roles/client_registration.html', {'form': form})
+
+
+def lawyer_registration(request):
+    if request.method == 'POST':
+        form = LawyerForm(request.POST)
+        if form.is_valid():
+            lawyer = form.save(commit=False)
+            lawyer.user = request.user
+            lawyer.save()
+            return redirect('list')
+    else:
+        form = LawyerForm()
+    return render(request, 'roles/lawyer_registration.html', {'form': form})
