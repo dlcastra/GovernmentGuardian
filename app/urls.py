0d2efc90d3b9474a0b9b7a438a3e2dc03dcc9ac7
dlcastra <1dlcastra@gmail.com>
2024-04-03 19:52:12 +0200

    Transferring URLs across different applications

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,6 @@ from app import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("get-list", views.lawyers_list, name="list"),
-    path("create/", views.add_lawyer, name="add_lawyer"),
-    path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout"),
-    path("callback", views.callback, name="callback"),
+    path("get-list/", views.lawyers_list, name="list"),
+    path("create/", views.add_lawyer, name="new-lawyer"),
 ]
--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("app.urls")),
+    path("", include("users.urls")),
     path("api/", include("api.urls")),
 ]
