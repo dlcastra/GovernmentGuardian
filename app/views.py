76fb48d20184e306b8fae0f9f63129c1a49c3772
dlcastra <1dlcastra@gmail.com>
2024-04-03 19:49:40 +0200

    Transferring the registration view to another application

--- app/views.py
+++ app/views.py
@@ -1,55 +1,13 @@
-from urllib.parse import quote_plus, urlencode
-
-from authlib.integrations.django_client import OAuth
-from django.conf import settings
 from django.shortcuts import render, redirect
-from django.urls import reverse
 
 from app.forms import LawyerForm
 from app.models import Lawyer
 
-""" --- SIGN UP AND SIGN IN --- """
-
-oauth = OAuth()
-
-oauth.register(
-    "auth0",
-    client_id=settings.AUTH0_CLIENT_ID,
-    client_secret=settings.AUTH0_CLIENT_SECRET,
-    client_kwargs={
-        "scope": "openid profile email",
-    },
-    server_metadata_url=f"https://{settings.AUTH0_DOMAIN}/.well-known/openid-configuration",
-)
+""" --- HOME PAGE --- """
 
 
 def index(request):
     return render(request, "wpage.html")
-
-
-def callback(request):
-    token = oauth.auth0.authorize_access_token(request)
-    request.session["user"] = token
-    return redirect(request.build_absolute_uri(reverse("list")))
-
-
-def login(request):
-    return oauth.auth0.authorize_redirect(request, request.build_absolute_uri(reverse("callback")))
-
-
-def logout(request):
-    request.session.clear()
-
-    return redirect(
-        f"https://{settings.AUTH0_DOMAIN}/v2/logout?"
-        + urlencode(
-            {
-                "returnTo": request.build_absolute_uri(reverse("index")),
-                "client_id": settings.AUTH0_CLIENT_ID,
-            },
-            quote_via=quote_plus,
-        ),
-    )
 
 
 """ --- CRUD FOR LAWYER --- """
