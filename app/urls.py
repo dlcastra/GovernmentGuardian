3849916da7f9a523c17227f25a4922d275742e1f
dlcastra <1dlcastra@gmail.com>
2024-04-03 14:34:54 +0200

    SIGN UP AND SIGN IN with Auth0

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,9 @@ from app import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create/", views.add_lawyer, name="add_lawyer")
+    path("get-list", views.lawyers_list, name="list"),
+    path("create/", views.add_lawyer, name="add_lawyer"),
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
+    path("callback", views.callback, name="callback"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,14 +1,61 @@
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.django_client import OAuth
+from django.conf import settings
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from app.forms import LawyerForm
 from app.models import Lawyer
 
+""" --- SIGN UP AND SIGN IN --- """
 
-def register(request):
-    ...
+oauth = OAuth()
+
+oauth.register(
+    "auth0",
+    client_id=settings.AUTH0_CLIENT_ID,
+    client_secret=settings.AUTH0_CLIENT_SECRET,
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f"https://{settings.AUTH0_DOMAIN}/.well-known/openid-configuration",
+)
 
 
 def index(request):
+    return render(request, "wpage.html")
+
+
+def callback(request):
+    token = oauth.auth0.authorize_access_token(request)
+    request.session["user"] = token
+    return redirect(request.build_absolute_uri(reverse("list")))
+
+
+def login(request):
+    return oauth.auth0.authorize_redirect(request, request.build_absolute_uri(reverse("callback")))
+
+
+def logout(request):
+    request.session.clear()
+
+    return redirect(
+        f"https://{settings.AUTH0_DOMAIN}/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": request.build_absolute_uri(reverse("index")),
+                "client_id": settings.AUTH0_CLIENT_ID,
+            },
+            quote_via=quote_plus,
+        ),
+    )
+
+
+""" --- CRUD FOR LAWYER --- """
+
+
+def lawyers_list(request):
     lawyers = Lawyer.objects.all()
     return render(request, "index.html", {"lawyers": lawyers})
 
@@ -24,3 +71,6 @@ def add_lawyer(request):
         return redirect("index")
 
     return render(request, "add_lawyer.html", {"form": form})
+
+
+""" --- CRUD FOR CLIENT ---"""
