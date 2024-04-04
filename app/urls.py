3b53eee86f9e14eb30cfaff6415676266459f8cd
dlcastra <1dlcastra@gmail.com>
2024-04-04 19:35:08 +0200

    Profiles for client and attorney have been added. Profiles support GET and POST methods where the latter is used to edit the profile

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ from app import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("get-list/", views.lawyers_list, name="list"),
-    path("create/", views.add_lawyer, name="new-lawyer"),
+    path("lawyer-profile/", views.LawyerProfile.as_view(), name="lawyer_profile"),
+    path("client-profile/", views.ClientProfile.as_view(), name="client_profile"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView
 
-from app.forms import LawyerForm
-from app.models import Lawyer
+from app.forms import LawyerForm, ClientForm
+from app.models import Lawyer, Client
 
 """ --- HOME PAGE --- """
 
@@ -10,25 +11,56 @@ def index(request):
     return render(request, "wpage.html")
 
 
-""" --- CRUD FOR LAWYER --- """
-
-
 def lawyers_list(request):
     lawyers = Lawyer.objects.all()
     return render(request, "index.html", {"lawyers": lawyers})
 
 
-def add_lawyer(request):
-    if request.method == "GET":
-        form = LawyerForm()
-        return render(request, "add_lawyer.html", {"form": form})
-
-    form = LawyerForm(request.POST)
-    if form.is_valid():
-        form.save()
-        return redirect("index")
-
-    return render(request, "add_lawyer.html", {"form": form})
+""" --- FUNCTIONS FOR LAWYER --- """
 
 
-""" --- CRUD FOR CLIENT ---"""
+class LawyerProfile(DetailView):
+    model = Lawyer
+    template_name = "profiles/lawyer/lawyer_profile.html"
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        lawyer = Lawyer.objects.get(user=user)
+        return render(request, self.template_name, {"lawyer": lawyer})
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        lawyer = Lawyer.objects.get(user=user)
+        form = LawyerForm(request.POST, instance=lawyer)
+
+        if "edit" in request.POST:
+            if form.is_valid():
+                form.save()
+                return redirect("lawyer_profile")
+
+        return render(request, "profiles/lawyer/edit_profile.html", {"lawyer": form})
+
+
+""" --- FUNCTIONS FOR CLIENT ---"""
+
+
+class ClientProfile(DetailView):
+    model = Client
+    template_name = "profiles/client/client_profile.html"
+
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        client = Client.objects.get(user=user)
+        return render(request, self.template_name, {"client": client})
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        client = Client.objects.get(user=user)
+        form = ClientForm(request.POST, instance=client)
+
+        if "edit" in request.POST:
+            if form.is_valid():
+                form.save()
+                return redirect("client_profile")
+
+        return render(request, "profiles/client/edit_profile.html", {"client": form})
